5a452a00f75340ea54ec396eaaeba6da978a0d3d
quenti77 <quentin.ysambert@gmail.com>
2023-05-08 15:04:52 +0200

    feat(main): first english translate for missing

---
+++ assets/api_request.md.fb4052a0.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as t,N as e}from"./chunks/framework.5e1c669d.js";const u=JSON.parse('{"title":"Request","description":"","frontmatter":{},"headers":[],"relativePath":"api/request.md"}'),o={name:"api/request.md"},l=e("",13),n=[l];function p(r,c,i,h,F,y){return t(),a("div",null,n)}const m=s(o,[["render",p]]);export{u as __pageData,m as default};
---
+++ assets/api_route.md.d4869f92.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,N as l}from"./chunks/framework.5e1c669d.js";const i=JSON.parse('{"title":"Route","description":"","frontmatter":{},"headers":[],"relativePath":"api/route.md"}'),o={name:"api/route.md"},p=l("",39),e=[p];function t(c,r,D,F,y,C){return n(),a("div",null,e)}const h=s(o,[["render",t]]);export{i as __pageData,h as default};
